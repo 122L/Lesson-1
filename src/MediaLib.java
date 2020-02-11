3b541e16dd666815f41b641d59413e7f92108034
122L <og153182@students.davenport.k12.ia.us>
2020-02-11 13:44:39 -0600

    bruh

--- src/MediaLib.java
+++ src/MediaLib.java
@@ -3,12 +3,16 @@ public class MediaLib {
         System.out.println("Welcome to your Media Library.");
 
         Song song1 = new Song();
-
         Books book1 = new Books();
+        Movies movie1 = new Movies();
 
-        System.out.println(song1);
-
-        song1.setTitle("Johnny B. Goode");
+        song1.setTitle("Bruh Sound Effect #2");
         System.out.println(song1.getTitle());
+
+        book1.setTitle("Minecraft Survival Handbook");
+        System.out.println(book1.getTitle());
+
+        movie1.setTitle("Tropic Thunder");
+        System.out.println(movie1.getTitle());
     }
 }
