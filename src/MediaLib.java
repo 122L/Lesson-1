f0b7941398ee53760fd592a10dd5ca3845ec7dee
122L <og153182@students.davenport.k12.ia.us>
2020-02-12 10:39:30 -0600

    bruh

--- src/MediaLib.java
+++ src/MediaLib.java
@@ -2,10 +2,35 @@ public class MediaLib {
     public static void main(String[] args) {
         System.out.println("Welcome to your Media Library.");
 
-        Song song1 = new Song("Respect", 1.29);
+        Song song1 = new Song("Bruh", 1.29, 5);
+        Song song2 = new Song("Respect", 1.29, 5);
+        Song song3 = new Song("Respect", 1.29, 5);
+        Song song4 = new Song("Respect", 1.29, 5);
+        Song song5 = new Song("Respect", 1.29, 5);
+        Song song6 = new Song("Respect", 1.29, 5);
+        Song song7 = new Song("Respect", 1.29, 5);
         Books book1 = new Books();
         Movies movie1 = new Movies();
 
-        System.out.println();
+        song1.setTitle("Bruh Sound Effect #2");
+        song1.setRating(5);
+        song1.setPrice(0.00);
+        System.out.println(song1.getTitle());
+        System.out.println(song1.getRating());
+        System.out.println(song1.getPrice());
+
+        book1.setTitle("Minecraft Survival Handbook");
+        book1.setRating(5);
+        book1.setPrice(6.00);
+        System.out.println(book1.getTitle());
+        System.out.println(book1.getRating());
+        System.out.println(book1.getPrice());
+
+        movie1.setTitle("Tropic Thunder");
+        movie1.setRating(5);
+        movie1.setPrice(3.00);
+        System.out.println(movie1.getTitle());
+        System.out.println(movie1.getRating());
+        System.out.println(movie1.getPrice());
     }
 }
--- src/Song.java
+++ src/Song.java
@@ -5,8 +5,30 @@ public class Song {
     private double price;
     private boolean favorite;
 
-    public Song(String title, double price) {
+    public Song(String title, double price, int rating) {
         this.title = title;
+        this.rating = rating;
         this.price = price;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String t) {
+        title = t;
+    }
+    public int getRating() {
+        return rating;
+    }
+    public void setRating(int r) {
+        rating = r;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double p) {
+        price = p;
+    }
+    public void addToFavorites() {
+        favorite = true;
     }
 }
