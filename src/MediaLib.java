a080b24e715c375ba8f7b60d7184ff1f49515d91
122L <og153182@students.davenport.k12.ia.us>
2020-02-13 13:06:42 -0600

    bruh

--- src/MediaLib.java
+++ src/MediaLib.java
@@ -1,41 +1,15 @@
 public class MediaLib {
     public static void main(String[] args) {
 
-        double totalCost;
-        int numberOfSongs = 0;
+        double totalCost = 0;
+        double averageRating = 0;
 
         System.out.println("Welcome to your Media Library.");
 
         Song song1 = new Song("Fellas in Paris", 5);
-        Song song2 = new Song("Ok and? Didn't ask", 5);
-        Song song3 = new Song("Metallica didn't play trapped under ice at the Freeze 'Em All concert", 5);
-        Song song4 = new Song("Billie Jean but the main melody never starts", 5);
-        Song song5 = new Song("The Fortnite Theme", 5);
-        Song song6 = new Song("Music that plays when unfortunate events happen", 5);
-        Song song7 = new Song("New Crispy Pretzel Chicken Fries At Burger King", 5);
+        Song song2 = new Song("Hello",1.50,7);
 
         Books book1 = new Books();
         Movies movie1 = new Movies();
-
-        song1.setTitle("Bruh Sound Effect #2");
-        song1.setRating(5);
-        song1.setPrice(0.00);
-        System.out.println(song1.getTitle());
-        System.out.println(song1.getRating());
-        System.out.println(song1.getPrice());
-
-        book1.setTitle("Minecraft Survival Handbook");
-        book1.setRating(5);
-        book1.setPrice(6.00);
-        System.out.println(book1.getTitle());
-        System.out.println(book1.getRating());
-        System.out.println(book1.getPrice());
-
-        movie1.setTitle("Tropic Thunder");
-        movie1.setRating(5);
-        movie1.setPrice(3.00);
-        System.out.println(movie1.getTitle());
-        System.out.println(movie1.getRating());
-        System.out.println(movie1.getPrice());
     }
 }
--- src/Song.java
+++ src/Song.java
@@ -4,21 +4,30 @@ public class Song {
     private int rating;
     private double price;
     private boolean favorite;
-    private int totalCost;
+    private static int numSongs;
 
-    public Song(String title, double price) {
-        this.title = title;
-        this.price = price;
+
+    public Song() {
+        title = " ";
+        rating = 0;
+        price = 0.0;
     }
-    public Song(String title, int rating, double price) {
-        this.title = title;
-        this.rating = rating;
-        this.price = price;
+    public Song(String t, double p) {
+        this.title = t;
+        this.price = p;
+    }
+    public Song(String t, double p, int r) {
+        this.title = t;
+        this.price = p;
+        this.rating = r;
+    }
     public String getTitle() {
         return title;
     }
-    public void setTitle(String t) {
+    public void setTitle(String t, double p, int r) {
         title = t;
+        price = p;
+        rating = r;
     }
     public int getRating() {
         return rating;
