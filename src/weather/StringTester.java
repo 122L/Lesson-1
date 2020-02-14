cd93bdedc79b5930c34253a7229058128270ad79
122L <og153182@students.davenport.k12.ia.us>
2020-02-14 13:05:49 -0600

    bruh

--- src/weather/StringTester.java
+++ src/weather/StringTester.java
@@ -1,4 +1,11 @@
 package weather;
 
 public class StringTester {
+    public static void main(String[] args) {
+        String weatherCondition = "not available";
+
+        System.out.println(weatherCondition.length());
+
+        System.out.println(weatherCondition.indexOf("thunder"));
+    }
 }
