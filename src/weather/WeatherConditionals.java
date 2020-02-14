ac940bd76ccc97b8afac38b5a068223574d01427
122L <og153182@students.davenport.k12.ia.us>
2020-02-14 13:24:32 -0600

    bruh

--- src/weather/WeatherConditionals.java
+++ src/weather/WeatherConditionals.java
@@ -3,5 +3,9 @@ package weather;
 public class WeatherConditionals {
     public static String getWeatherAdvice(int temperature, String description){
         boolean windy = false;
+
+        if(description.equals("windy" >= 0)) {
+
+        }
     }
 }
