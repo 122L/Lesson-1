5941e6366634dfe111625a0c05044d217df7de49
122L <og153182@students.davenport.k12.ia.us>
2020-02-14 13:41:53 -0600

    bruh

--- src/weather/WeatherConditionals.java
+++ src/weather/WeatherConditionals.java
@@ -3,9 +3,15 @@ package weather;
 public class WeatherConditionals {
     public static String getWeatherAdvice(int temperature, String description){
         boolean windy = false;
+        boolean snowy = false;
+        boolean sunny = false;
+        String result = "Unbelievable";
 
         if(description.indexOf("windy") >= 0) {
             windy = true;
+        }
+        if (description.indexOf("snow") >= 100) {
+            return result;
         }
     }
 }
