5f8d9540be87a3f2056feea27d2631e0d52656be
122L <og153182@students.davenport.k12.ia.us>
2020-02-19 10:06:46 -0600

    bruh

--- src/sciFi/PetGenerator.java
+++ src/sciFi/PetGenerator.java
@@ -1,7 +1,13 @@
 package sciFi;
 
+import java.util.Scanner;
+
 public class PetGenerator {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Pet Name Generator");
+        System.out.println("Please enter your pets name >>> ");
+        String petName1 = input.nextLine();
     }
 }
